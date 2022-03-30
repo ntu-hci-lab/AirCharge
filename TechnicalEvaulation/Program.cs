92873ec3d7456ce2be1bc7169cace6a7c8fb3ecc
scl971009 <dora20823@gmail.com>
2022-03-31 03:04:50 +0800

    Upload remain C# codes

---
+++ TechnicalEvaulation/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Ports;
+using System.Management;
+using System.Threading;
+
+namespace IMADA_Force_Measure
+{
+    class Program
+	{
+		static void Main()
+        {
+			FinalForce function = new FinalForce();
+			function.Start();
+		}
+	}
+}
